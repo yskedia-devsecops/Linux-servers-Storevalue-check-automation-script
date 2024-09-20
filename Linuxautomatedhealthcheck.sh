dcba5dc802f534d21924528e9c4aaa9360e01a15
yskedia-devsecops <kediayash123@gmail.com>
2024-09-21 02:40:23 +0530

    Update and rename Description.txt to Linuxautomatedhealthcheck.sh

--- Linuxautomatedhealthcheck.sh
+++ Linuxautomatedhealthcheck.sh
@@ -10,3 +10,33 @@ To address this operational inefficiency, the Engineering Team developed an auto
 
 Result : 
 The implementation of the Python script transformed the health check process, reducing the assessment time to under a minute per server. This efficiency gain cut down the total daily commitment to less than half an hour, freeing up valuable technical resources and minimizing the margin for error. The automation has not only bolstered operational productivity but also reinforced the reliability of the SOC's monitoring capabilities, ensuring the steadfast performance of QRadar systems.
+
+Find the code below : 
+Make Directory scripts_healthcheck , create healthcheck.sh script on linux server and give chmod +x healthcheck.sh (CLI of QRadar , EC / EP) then vi healthcheck.sh
+then copy and paste below code : close with Cntrl + ESC wq! And finally run the script using sh cortex.sh
+
+#!/bin/bash
+
+#All server IPs dynamically from /opt/qradar/support/all_servers.sh
+SERVER_IPS=$(/opt/qradar/support/all_servers.sh | awk '{print $1}')
+
+# Get current date and time to create the output file
+OUTPUT_FILE="$(date +'%Y-%m-%d_%H-%M').txt"
+
+# Create or clear the output file
+> "$OUTPUT_FILE"
+
+# Function to fetch /store % value using df -h
+fetch_store_info() {
+    local HOST=$1
+
+    # Fetch the /store usage percentage and append to the output file
+    ssh -o BatchMode=yes -o ConnectTimeout=5 "$HOST" "df -h /store | awk 'NR==2 {print \$5}'" 2>/dev/null | awk -v host="$HOST" '{print host, $0}' >> "$OUTPUT_FILE"
+}
+
+# Loop through all the server IPs and fetch /store % value for each
+for HOST in $SERVER_IPS; do
+    fetch_store_info "$HOST"
+done
+
+echo "All /store % values fetched and saved to $OUTPUT_FILE"
